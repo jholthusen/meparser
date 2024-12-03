5bb5202b51a9ab4d98c64f247690622d26cc1c3f
Jan Holthusen <jan@holthusen.de>
2024-12-03 14:21:25 +0100

    initial setup

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Parse PDF file and build necessary objects.
+$parser = new \Smalot\PdfParser\Parser();
+$pdf = $parser->parseFile('/path/to/document.pdf');
+
+$text = $pdf->getText();
+echo $text;
+
+?>
