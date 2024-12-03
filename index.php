a07f7d2402ad8bf9ae5adec1daf2fdb5085fe346
Jan Holthusen <jan@holthusen.de>
2024-12-03 14:38:43 +0100

    first working prototype(local php)

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 
 // Parse PDF file and build necessary objects.
 $parser = new \Smalot\PdfParser\Parser();
-$pdf = $parser->parseFile('/path/to/document.pdf');
+$pdf = $parser->parseFile('C:\Users\holthusen\OneDrive - adesso Group\Dokumente\dev\meparser\meparser\241207-Duisburg-Duisburger_ST.pdf');
 
 $text = $pdf->getText();
 echo $text;
