368b2ca88e0adfb4c31e3e69cd843e83c458d0b9
Jan Holthusen <jan@holthusen.de>
2024-12-03 14:40:53 +0100

    relative path

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 // Parse PDF file and build necessary objects.
 $parser = new \Smalot\PdfParser\Parser();
-$pdf = $parser->parseFile('C:\Users\holthusen\OneDrive - adesso Group\Dokumente\dev\meparser\meparser\241207-Duisburg-Duisburger_ST.pdf');
+$pdf = $parser->parseFile('./241207-Duisburg-Duisburger_ST.pdf');
 
 $text = $pdf->getText();
 echo $text;
